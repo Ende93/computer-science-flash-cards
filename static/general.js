3e7f3b2468447f17a92233fe7e3cf89bbb2d1e00
ende93 <ende93@hotmail.com>
2020-04-29 18:06:28 +0800

    refactor: use css instead of js to control radio

--- static/general.js
+++ static/general.js
@@ -27,36 +27,6 @@ $(document).ready(function(){
         });
     }
 
-    if ($('.editPanel').length != 0) {
-
-        function checkit() {
-            var checkedVal = $('input[name=type]:checked').val();
-            if (checkedVal === undefined) {
-                // hide the fields
-                $('.fieldFront').hide();
-                $('.fieldBack').hide();
-                $('.saveButton').hide();
-            } else {
-                $('.toggleButton').removeClass('toggleSelected');
-                $(this).addClass('toggleSelected');
-
-                if (checkedVal == '1') {
-                    $('textarea[name=back]').attr('rows', 5);
-                } else {
-                    $('textarea[name=back]').attr('rows', 12);
-                }
-
-                $('.fieldFront').show();
-                $('.fieldBack').show();
-                $('.saveButton').show();
-            }
-        }
-
-        $('.toggleButton').click(checkit);
-
-        checkit();
-    }
-
     // to remove the short delay on click on touch devices
     FastClick.attach(document.body);
 });
